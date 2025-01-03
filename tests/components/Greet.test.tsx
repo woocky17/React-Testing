1525bff8c914bc55289aa725dd238c521e8d65fc
David Ortet <davidortetsabater@gmail.com>
2025-01-03 11:15:07 +0100

    Simplify Test Setup

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,7 +1,5 @@
-import { test, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Greet from "../../src/components/Greet";
-import "@testing-library/jest-dom/vitest";
 
 describe("Greet", () => {
   test("should render login button when is not provided", () => {
