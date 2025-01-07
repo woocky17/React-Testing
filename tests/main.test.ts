bfe3dedef181985b91a8e5ac6708516f4b3e6709
David Ortet <davidortetsabater@gmail.com>
2025-01-07 11:11:07 +0100

    Set Up Mock Service

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,11 @@
 import { test, expect, describe } from "vitest";
 
 describe("test suite", () => {
-  test("should", () => {
+  test("should", async () => {
+    const response = fetch("/categories");
+    const data = await (await response).json();
+    console.log(data);
+
     expect(1).toBe(1);
   });
 });
