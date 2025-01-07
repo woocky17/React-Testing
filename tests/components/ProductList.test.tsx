cd4c29b2c52e96bb97a7e12681f89c28ca0aeff1
David Ortet <davidortetsabater@gmail.com>
2025-01-07 13:27:43 +0100

    Test the Loading State From ProductList

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -1,7 +1,12 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import ProductList from "../../src/components/ProductList";
 import { server } from "../mocks/server";
-import { http, HttpResponse } from "msw";
+import { http, HttpResponse, delay } from "msw";
 import { db } from "../mocks/db";
 
 describe("ProductList", () => {
@@ -51,5 +56,35 @@ describe("ProductList", () => {
 
     const error = await screen.findByText(/error/i);
     expect(error).toBeInTheDocument();
+  });
+
+  test("should render a loading indicator when is loading", async () => {
+    server.use(
+      http.get("/products", async () => {
+        await delay();
+        return HttpResponse.json([]);
+      })
+    );
+
+    render(<ProductList />);
+
+    expect(await screen.findByText(/loading/i)).toBeInTheDocument();
+  });
+
+  test("should remove the loading indicator once is finish loading", async () => {
+    render(<ProductList />);
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+  });
+
+  test("should remove the loading indicator if the loading fails", async () => {
+    server.use(
+      http.get("/products", () => {
+        return HttpResponse.error();
+      })
+    );
+    render(<ProductList />);
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
   });
 });
