7243b2d03cb40b1d46bfcc1f032b453d2889b921
David Ortet <davidortetsabater@gmail.com>
2025-01-09 10:37:36 +0100

    Extract Function for Filling Forms

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -24,16 +24,52 @@ describe("ProductForm", () => {
       wrapper: AllProviders,
     });
 
+    const user = userEvent.setup();
+
     return {
+      expectErrorToBeInTheDocument: (errorMessage: RegExp) => {
+        const error = screen.getByRole("alert");
+        expect(error).toBeInTheDocument;
+        expect(error).toHaveTextContent(errorMessage);
+      },
+
       getInput: async () => {
         await screen.findByRole("form");
+
+        const comboBox = screen.getByRole("combobox", { name: /category/i });
+        const name = screen.getByPlaceholderText(/name/i);
+        const price = screen.getByPlaceholderText(/price/i);
+        const submitButton = screen.getByRole("button");
+
+        type FormData = { [K in keyof Product]: any };
+
+        const validData: FormData = {
+          id: 1,
+          name: "a",
+          price: 1,
+          categoryId: 1,
+        };
+
+        const fill = async (product: FormData) => {
+          if (product.name !== undefined) await user.type(name, product.name);
+          if (product.price !== undefined)
+            await user.type(price, product.price.toString());
+
+          await user.click(comboBox);
+          const option = screen.getAllByRole("option");
+          await user.click(option[0]);
+          await user.click(submitButton);
+        };
         return {
-          comboBox: screen.getByRole("combobox", { name: /category/i }),
-          name: screen.getByPlaceholderText(/name/i),
-          price: screen.getByPlaceholderText(/price/i),
+          comboBox,
+          name,
+          price,
+          submitButton,
+          fill,
+          validData,
         };
       },
-      user: userEvent.setup(),
+      user,
     };
   };
   test("should render form fields", async () => {
@@ -77,4 +113,65 @@ describe("ProductForm", () => {
     const { name } = await getInput();
     expect(name).toHaveFocus();
   });
+
+  test.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "longer than 255 characters",
+      name: "a".repeat(256),
+      errorMessage: /255/i,
+    },
+  ])(
+    "should display an error if name is $scenario",
+    async ({ name, errorMessage }) => {
+      const { getInput, expectErrorToBeInTheDocument } = renderProductForm();
+
+      const form = await getInput();
+      const product = { ...form.validData, name };
+      await form.fill(product);
+
+      expectErrorToBeInTheDocument(errorMessage);
+    }
+  );
+
+  test.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/,
+    },
+    {
+      scenario: "greater than 1000",
+      price: 1001,
+      errorMessage: /1000/,
+    },
+    {
+      scenario: "0",
+      price: 0,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "less than 0",
+      price: -1,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "NaN",
+      price: "a",
+      errorMessage: /required/,
+    },
+  ])(
+    "should display an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { getInput, expectErrorToBeInTheDocument } = renderProductForm();
+
+      const form = await getInput();
+      const product = { ...form.validData, price };
+      await form.fill(product);
+
+      expectErrorToBeInTheDocument(errorMessage);
+    }
+  );
 });
