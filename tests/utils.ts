9d92512bacccc0f6128fc201fd532a86cad96e11
David Ortet <davidortetsabater@gmail.com>
2025-01-10 07:58:44 +0100

    Mock of the Authentication Status

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,4 +1,5 @@
-import { http, HttpResponse, delay } from "msw";
+import { useAuth0, User } from "@auth0/auth0-react";
+import { delay, http, HttpResponse } from "msw";
 import { server } from "./mocks/server";
 
 export const simulateDelay = (endpoint: string) => {
@@ -17,4 +18,23 @@ export const simulateError = (endpoint: string) => {
       return HttpResponse.error();
     })
   );
+};
+
+type AuthState = {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  user: User | undefined;
+};
+
+export const mockAuthState = (authState: AuthState) => {
+  vi.mocked(useAuth0).mockReturnValue({
+    ...authState,
+    getAccessTokenSilently: vi.fn(),
+    getAccessTokenWithPopup: vi.fn(),
+    getIdTokenClaims: vi.fn(),
+    loginWithRedirect: vi.fn(),
+    loginWithPopup: vi.fn(),
+    logout: vi.fn(),
+    handleRedirectCallback: vi.fn(),
+  });
 };
